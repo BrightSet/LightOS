bb831000ebe815bbbfb6cad1268eb759f0a6ab39
Felix Kranich <felix.kranich@me.com>
2014-03-28 22:18:37 +0100

    Tweaked settings apps's interface and removed bootstrap

--- apps/applib.js
+++ apps/applib.js
@@ -1,5 +1,8 @@
-document.getElementsByTagName("head")[0].innerHTML += "<style>* {margin:0;}</style>";
+document.getElementsByTagName("head")[0].innerHTML += "<style>body:not(#mainBar) { margin:12pt; }</style>";
 document.getElementsByTagName("head")[0].innerHTML += "<link rel='stylesheet' href='../clean.css' type='text/css'/>";
 //document.getElementsByTagName("head")[0].innerHTML += "<link rel='stylesheet' href='.../style.css' type='text/css'/>";
-document.writeln("<div id='mainBar'><span>&#8617;</span><span onclick='location.href=\"../index.html\"' style='position:fixed; margin-left:45%;' ontouchstart='location.href=\"../index.html\"'>&#8962;</span><span style='margin-left:90%' onclick='location.href=\"../applauncher.html\"' ontouchstart='location.href=\"../applauncher.html\"'>&#9992;</span></div>");
+if(!document.getElementsByTagName("html")[0].className.match("noMainBar"))
+{
+	document.writeln("<div id='mainBar' class='mainBar' style='margin:0;'><span>&#8617;</span><span onclick='location.href=\"../index.html\"' style='position:fixed; margin-left:45%;' ontouchstart='location.href=\"../index.html\"'>&#8962;</span><span style='margin-left:90%' onclick='location.href=\"../applauncher.html\"' ontouchstart='location.href=\"../applauncher.html\"'>&#9992;</span></div>");
+}
 $("*").toggleClass("cursor");
